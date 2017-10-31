4617689ce7fee3e25651a5c9e98cf718ef6763b6
Yixian <duyixian1234@qq.com>
2017-10-31 13:18:57 +0800

    初步完成

---
+++ setup.py
@@ -0,0 +1,14 @@
+'''the config of pypi.'''
+from setuptools import setup
+setup(
+    name='qcloudapi',
+    packages=['qcloudapi'],
+    version='0.2',
+    description='To make a signature with the hmac algorithm. ',
+    author='Yixian Du',
+    author_email='duyixian1233@outlook.com',
+    url='https://github.com/duyixian1234/qcloudapi',
+    keywords=['qcloud'],
+    install_requires=['hmac_signature'],
+    classifiers=[],
+)
